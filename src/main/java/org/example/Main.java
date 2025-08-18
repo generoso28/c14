7bb79c2aa87cc31f8245820e5c7be9efe4364877
Lucas Padua <lspadua41@gmail.com>
2025-08-18 20:22:39 -0300

    alteração de variavel file -> caminho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/main/java/assets/acampamento.pdf");
-        try (PDDocument document = Loader.loadPDF(file)) {
+        File caminho = new File("src/main/java/assets/acampamento.pdf");
+        try (PDDocument document = Loader.loadPDF(caminho)) {
 
             if (document.isEncrypted()) {
                 System.err.println("Erro: O documento está criptografado e não pode ser lido.");
